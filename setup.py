5b71d7cdee9c7a4122c0a72a469831d9c07f28c4
Sivanandareddyseelam <you@example.com>
2023-05-15 23:22:20 +0530

    all files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name = "census-income",
+      version = "0.0.1",
+      author = "siva",
+      author_mail = "siva@gmal.com",
+      package = find_packages(),
+      install_requires = ["pandas","numpy"]  
+    
+)
